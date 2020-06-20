7dafc3da81bec96c50d0c1e13dc3bf6389958e4f
rashijhawar <56639538+rashijhawar@users.noreply.github.com>
2020-06-20 13:41:08 +0530

    Map

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/map.html'))});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
