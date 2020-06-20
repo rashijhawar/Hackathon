e85003e32fd2fd4037d78096ca521e1a85f4e239
rashijhawar <56639538+rashijhawar@users.noreply.github.com>
2020-06-20 14:38:35 +0530

    Form and post

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/map.html'))});
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/form.html'))});
+
+app.post('/', function(req, res){
+    console.log(`Start Location: ${req.body.start} and End Location: ${req.body.end}`);
+    res.send("Form successfully submitted!");
+
+    const start = req.body.start.replace(/ /g, "+");
+    const end = req.body.end.replace(/ /g, "+");
+
+    request('http://open.mapquestapi.com/geocoding/v1/address?key=3RPKDErBGRFQ7bnjN2Pl0YSYGQ61iWnw&location=start', 
+    function(error, res, body) {
+        console.log(res.body);
+        //console.log(res.json.results[0].locations[0].displayLatLng.lat);
+        //console.log(res.body.results[0].locations[0].displayLatLng.lng);
+    });
+ });
+
 
 const PORT = process.env.PORT || 5000;
 
